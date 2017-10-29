7c679cfa10af53db5599a2e267dc77d007b6706e
박정재 <jung-jae.park@nhn.com>
2017-10-29 22:35:11 +0900

    x

--- Codility/src/com/example/codility/Main.java
+++ Codility/src/com/example/codility/Main.java
@@ -1,15 +1,40 @@
 package com.example.codility;
 
-import com.example.codility.lesson3.TapeEquilibrium;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+class Main {
 
 	public static void main(String[] args) {
-		TapeEquilibrium problem = new TapeEquilibrium();
-		
-		int[] A = {-1000,1000};
-		
-		System.out.println(problem.solution(A));
-	}
+		try (Scanner s = new Scanner(System.in)) {
+			int n = s.nextInt();
+			int d = s.nextInt();
+			int k = s.nextInt();
+			int j = s.nextInt();
 
+			List<Integer> baejjang = new LinkedList<>();
+			int index;
+
+			if (d == 1) {
+				for (int i = 1; i <= n; i++) {
+					baejjang.add(i);
+				}
+				index = 1;
+			} else {
+				for (int i = n; i >= 1; i--) {
+					baejjang.add(i);
+				}
+				index = 0;
+			}
+
+			while (baejjang.size() != 1) {
+				index = (index + k - 1) % baejjang.size();
+				baejjang.remove(index);
+				k += j;
+			}
+
+			System.out.println(baejjang.get(0));
+		}
+	}
 }
